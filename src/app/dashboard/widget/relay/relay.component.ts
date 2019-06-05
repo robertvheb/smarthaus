c207302b25c900acadb2de9f55eb40f763f9bdf3
Robert Heb <robertv.heb@gmail.com>
2019-06-05 17:33:07 +0300

    relay as function

--- src/app/dashboard/widget/relay/relay.component.ts
+++ src/app/dashboard/widget/relay/relay.component.ts
@@ -23,11 +23,13 @@ export class RelayComponent implements OnInit, OnDestroy {
   }
   
   onRelay() {
-    this.deviceService.digitalWrite(this.relayDevice.id, this.pin, 1);
+    //this.deviceService.digitalWrite(this.relayDevice.id, this.pin, 1);
+    this.deviceService.callFunction(this.relayDevice.id, 'relay_on', this.pin);
   }
 
   offRelay() {
-    this.deviceService.digitalWrite(this.relayDevice.id, this.pin, 0);
+    //this.deviceService.digitalWrite(this.relayDevice.id, this.pin, 0);
+    this.deviceService.callFunction(this.relayDevice.id, 'relay_off', this.pin);
   }
 }
 
